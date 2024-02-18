2e0a88e2d35b024447e7dcea14fb6c70b61dbfa8
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-02-17 21:05:35 -0500

    Fix grid styling
    Finally got the grid items to fit neatly inside the container without
    weird spacing irregularities.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const gridContainer = document.querySelector('#grid-container');
 console.log(gridContainer);
-
-for (let i = 0; i < 16*16; i++){
+for (let i = 0; i < 256; i++){
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
+    // gridItem.style.height = ""
     gridContainer.appendChild(gridItem);
 }
