44ed55f4f23bfa8eb28783493d88d5c4a23b679a
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-02-20 17:48:00 -0500

    Add color change on hover event
    Grid divs change to black when mouseenter

--- script.js
+++ script.js
@@ -4,5 +4,15 @@ for (let i = 0; i < 256; i++){
     const gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
     // gridItem.style.height = ""
-    gridContainer.appendChild(gridItem);
+    gridContainer.appendChild(gridItem);    
 }
+
+const gridItems = document.querySelectorAll('.grid-item');
+const gridItem = document.querySelector ('.grid-items');
+function hoverColor(e){
+    e.target.style.backgroundColor = 'black';
+}
+
+gridItems.forEach((gridItem) => {
+  gridItem.addEventListener('mouseenter', hoverColor);
+});
