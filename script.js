973a91ced65abd27c080a3d1733430f32ac42abf
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-02-23 16:31:23 -0500

    Add title and grid generator button, plus styles
    Added new DOM elements to set up the grid generation function
    and organize the page

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const pageName = document.createElement('h1');
+pageName.setAttribute('id', 'page-name');
+document.body.appendChild(pageName);
+document.querySelector('#page-name').textContent = 'Etch-a-Sketch (kinda)';
+const gridBtn = document.createElement('button');
+gridBtn.setAttribute('id', 'grid-button');
+pageName.appendChild(gridBtn);
+document.querySelector('#grid-button').textContent = 'Generate grid';
+
+
 const gridContainer = document.querySelector('#grid-container');
 console.log(gridContainer);
 for (let i = 0; i < 256; i++){
@@ -8,7 +18,7 @@ for (let i = 0; i < 256; i++){
 }
 
 const gridItems = document.querySelectorAll('.grid-item');
-const gridItem = document.querySelector ('.grid-items');
+const gridItem = document.querySelector ('.grid-item');
 function hoverColor(e){
     e.target.style.backgroundColor = 'black';
 }
