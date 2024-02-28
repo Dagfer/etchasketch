977331f0718b8f7376da2b4bd4d50628c63400af
Dagfer <137955962+Dagfer@users.noreply.github.com>
2024-02-28 18:10:17 -0500

    Add listener and functions to generate grid btn
    wrote functions to remove grid and generate a new one
    using prompt input. Added new CSS selectors to fix
    styling issues.

--- script.js
+++ script.js
@@ -2,13 +2,15 @@ const pageName = document.createElement('h1');
 pageName.setAttribute('id', 'page-name');
 document.body.appendChild(pageName);
 document.querySelector('#page-name').textContent = 'Etch-a-Sketch (kinda)';
+
 const gridBtn = document.createElement('button');
 gridBtn.setAttribute('id', 'grid-button');
 pageName.appendChild(gridBtn);
 document.querySelector('#grid-button').textContent = 'Generate grid';
-
+gridBtn.addEventListener ('click', generateGrid)
 
 const gridContainer = document.querySelector('#grid-container');
+gridContainer.classList.add('container');
 console.log(gridContainer);
 for (let i = 0; i < 256; i++){
     const gridItem = document.createElement('div');
@@ -18,7 +20,7 @@ for (let i = 0; i < 256; i++){
 }
 
 const gridItems = document.querySelectorAll('.grid-item');
-const gridItem = document.querySelector ('.grid-item');
+
 function hoverColor(e){
     e.target.style.backgroundColor = 'black';
 }
@@ -26,3 +28,39 @@ function hoverColor(e){
 gridItems.forEach((gridItem) => {
   gridItem.addEventListener('mouseenter', hoverColor);
 });
+
+function removeGrid(){
+  let  inputPrompt = prompt('how many cells per side?');
+  console.log(typeof inputPrompt);
+  if (typeof parseInt(inputPrompt) === 'number'){
+      const containers = document.querySelectorAll('.container');
+      containers.forEach((container) => {
+      container.remove();
+    });
+      return cellNum = parseInt(inputPrompt);
+   }
+}
+
+function generateGrid(){
+    removeGrid()
+    const gridContainerTwo = document.createElement('div');
+    gridContainerTwo.classList.add('container')
+    gridContainerTwo.setAttribute('id', '#grid-containertwo');
+    document.body.appendChild(gridContainerTwo);
+    console.log(gridContainerTwo);
+    const bodyDouble = document.querySelector('body');
+    bodyDouble.classList.add('body-double');
+
+    for (let i = 0; i < cellNum ** 2; i++){
+        const gridItem = document.createElement('div');
+        gridItem.classList.add('grid-item');
+        gridContainerTwo.appendChild(gridItem);    
+    }
+    const gridItems = document.querySelectorAll('.grid-item');
+
+    gridItems.forEach((gridItem) => {
+        gridItem.addEventListener('mouseenter', hoverColor);
+      });
+      
+}
+
